a86da9001d7ff881f9278ce70985d8c96aa6e7c5
Admin <patilsharad2000@gmail.co>
2021-02-26 13:34:44 +0530

    added: Creating Contact in AddressBOOK  First name , last name and Address etc.

--- src/main/java/com/addressbook/javapractice/AddressBook.java
+++ src/main/java/com/addressbook/javapractice/AddressBook.java
@@ -1,7 +1,27 @@
 package com.addressbook.javapractice;
 
 public class AddressBook {
-    public static void main(String[] args) {
-        System.out.println("Welocome to Address Book Program");
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    int zipCode;
+    String phoneNumber;
+
+    AddressBook(String firstName, String lastName, String address, String city, String state, int zipCode,String phoneNumber)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
     }
-}
+
+    public static void main(String[] args)
+    {
+        AddressBook ab = new AddressBook("Sharad","Patil","Nyahalod","Dhule","Maharastra",424002,"9096549959")
+    }
+}}
